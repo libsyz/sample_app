ad5d41ce18e62d6fbcec5a00e86645493e5e08db
Miguel Jimenez <denkatalabeno@gmail.com>
2017-07-11 16:13:59 +0200

    title helper added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 
   test "should get root" do
-
     get root_url
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
