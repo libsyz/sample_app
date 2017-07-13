e832cd05d47ef04e0ff41c2904e478a79f8a38e9
Miguel Jimenez <denkatalabeno@gmail.com>
2017-07-13 12:51:37 +0200

    integration test for sign up form done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '/home'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit!
+  end
+
 end
